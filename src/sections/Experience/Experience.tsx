107cb157c570b0f30b625acceb62f44fecd9fa9e
Trina <trina.sikdar.2101@gmail.com>
2024-06-19 21:14:14 +0530

    .

--- src/sections/Experience/Experience.tsx
+++ src/sections/Experience/Experience.tsx
@@ -5,9 +5,6 @@ import styles from './ExperienceStyles.module.css'
 const Experience = () => {
 	const experienceList: MyExperience[] = [
 		{ company: "Wipro", role: "Developer", startDate: new Date('2024-07-19'), endDate: "Present", description: "Developed web applications using React and Node.js" },
-		{ company: "Wipro", role: "Developer", startDate: new Date('2024-07-19'), endDate: "Present", description: "Developed web applications using React and Node.js" },
-		{ company: "Wipro", role: "Developer", startDate: new Date('2024-07-19'), endDate: "Present", description: "Developed web applications using React and Node.js" },
-		{ company: "Wipro", role: "Developer", startDate: new Date('2024-07-19'), endDate: "Present", description: "Developed web applications using React and Node.js" },
 	];
 
 	return (
