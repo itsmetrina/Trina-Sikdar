31df7d9c3755f344ac6c688028c3ce9fb4368f55
Trina <trina.sikdar.2101@gmail.com>
2024-06-20 17:29:15 +0530

    .

--- src/sections/Star/Star.tsx
+++ src/sections/Star/Star.tsx
@@ -1,20 +1,11 @@
 import styles from "./StarStyles.module.css";
 
 import starImg from "../../assets/hero-img.png";
-
-import githubLight from "../../assets/github-light.svg";
-import githubDark from "../../assets/github-dark.svg";
-import linkedinLight from "../../assets/linkedin-light.svg";
-import linkedinDark from "../../assets/linkedin-dark.svg";
-import gmailLight from "../../assets/gmail-light.png";
-import gmailDark from "../../assets/gmail-dark.png";
 import CV from "../../assets/cv.pdf";
 
-import { useTheme } from "../../common/ThemeContext";
 import { ReactTyped } from "react-typed";
 import { motion } from "framer-motion";
-
-import { IoMdMoon, IoMdSunny } from "react-icons/io";
+import { FaAt, FaGithub, FaLinkedin } from "react-icons/fa";
 
 const leftFlowIn = (delay: number) => ({
 	hidden: { x: -100, opacity: 0 },
@@ -22,17 +13,6 @@ const leftFlowIn = (delay: number) => ({
 });
 
 const Star = () => {
-	const { theme, toggleTheme } = useTheme() as unknown as {
-		theme: string;
-		toggleTheme: () => void;
-	};
-
-	const themeIcon =
-		theme === "light" ? <IoMdSunny size={30} /> : <IoMdMoon size={30} />;
-	const githubIcon = theme === "light" ? githubLight : githubDark;
-	const linkedinIcon = theme === "light" ? linkedinLight : linkedinDark;
-	const gmailIcon = theme === "light" ? gmailLight : gmailDark;
-
 	return (
 		<section id="star" className={styles.container}>
 			<div className={styles.colorModeContainer}>
@@ -44,14 +24,6 @@ const Star = () => {
 					animate={{ x: 0, opacity: 1 }}
 					transition={{ duration: 1, delay: 1.2 }}
 				/>
-				<motion.span
-					className={styles.colorMode}
-					onClick={toggleTheme}
-					whileHover={{ scale: 1.1 }}
-					whileTap={{ scale: 0.9 }}
-				>
-					{themeIcon}
-				</motion.span>
 			</div>
 			<div className={styles.info}>
 				<motion.h1 variants={leftFlowIn(0)} initial="hidden" animate="visible">
@@ -84,13 +56,13 @@ const Star = () => {
 					animate="visible"
 				>
 					<a href="https://github.com" target="_blank">
-						<img src={githubIcon} alt="Github Icon" />
+						<FaGithub />
 					</a>
 					<a href="https://linkedIn.com" target="_blank">
-						<img src={linkedinIcon} alt="LinkedIn Icon" />
+						<FaLinkedin />
 					</a>
 					<a href="mailto:trinasikdar2000@gmail.com" target="_blank">
-						<img src={gmailIcon} alt="Gmail Icon" />
+						<FaAt />
 					</a>
 				</motion.span>
 				<motion.p
